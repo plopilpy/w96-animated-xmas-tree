233fcecdd06ada957251af10ddfab73fc57265c9
plopilpy <70001470+plopilpy@users.noreply.github.com>
2021-12-14 01:21:35 -0500

    Add files via upload

---
+++ dist/a/animated-xmas-tree/content@1.0/scripts/install.js
@@ -0,0 +1,1 @@
+w96.ui.DialogCreator.alert("Happy Holidays 2021 ! Thank you for installing my package.\n I hope you enjoy it, as it took quite some effort. Please reboot to load the addon properly. -Plopilpy :)");
